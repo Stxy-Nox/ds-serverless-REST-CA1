4798ae2e929afd9fa863ad5b7b2ca7a92645bb9e
SytxNox <seven_night_w@hotmail.com>
2025-03-18 12:56:45 +0000

    Feat:Implement table type and add seed data

---
+++ shared/types.d.ts
@@ -0,0 +1,10 @@
+export type Game = {
+  game_id: number,
+  name: string,
+  platform: string,
+  release_date: string,
+  summary: string,
+  meta_score: number,
+  user_review: number,
+  isEighthGen: boolean
+}
