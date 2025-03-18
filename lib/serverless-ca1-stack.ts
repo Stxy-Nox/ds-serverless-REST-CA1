8366f052c90b92b9216fc777ddc11e3c4d5f44f8
SytxNox <seven_night_w@hotmail.com>
2025-03-18 11:52:10 +0000

    Feat: Implement test funtion

--- lib/serverless-ca1-stack.ts
+++ lib/serverless-ca1-stack.ts
@@ -1,16 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambdanode from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as custom from "aws-cdk-lib/custom-resources";
+import * as apig from "aws-cdk-lib/aws-apigateway";
 
 export class ServerlessCa1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //Tables
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServerlessCa1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    //Funcitons
+    const testFn = new lambdanode.NodejsFunction(this, "TestFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_20_X,
+      entry: `${__dirname}/../lambdas/test.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+    })
+
+
+    //Permissions
+
+    //REST API
+
+    //Endpoints
   }
 }
