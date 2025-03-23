4eb6b824135ee382617b1b2babbe7b6c5d6eeb06
SytxNox <seven_night_w@hotmail.com>
2025-03-23 23:13:56 +0000

    Feat(api): Implement add new game POST endpoint

--- lib/serverless-ca1-stack.ts
+++ lib/serverless-ca1-stack.ts
@@ -70,6 +70,18 @@ export class ServerlessCa1Stack extends cdk.Stack {
       }
       );
 
+    const newGameFn = new lambdanode.NodejsFunction(this, "AddGameFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_20_X,
+      entry: `${__dirname}/../lambdas/addGame.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: gamesTable.tableName,
+        REGION: "eu-west-1",
+      },
+    });
+
     new custom.AwsCustomResource(this, "gamesddbInitData", {
       onCreate: {
         service: "DynamoDB",
