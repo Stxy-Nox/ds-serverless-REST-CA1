34950b0e7d4018ad9fbf9db585375b521ec169aa
SytxNox <seven_night_w@hotmail.com>
2025-03-24 12:35:12 +0000

    feat: install aws-sdk

--- lambdas/translate.ts
+++ lambdas/translate.ts
@@ -1,7 +1,23 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import apiResponses from './common/apiResponses';
+import * as AWS from 'aws-sdk';
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const body = JSON.parse(event.body);
+  const { text, language } = body;
+  const translate = new AWS.Translate();
+
+  if (!text) {
+    return apiResponses._400({ message: 'missing text fom the body' });
+  }
+  if (!language) {
+    return apiResponses._400({ message: 'missing language from the body' });
+  }
+
+  try{
+
+  }catch(error){
     
-  export const handler: APIGatewayProxyHandler = async (event) => {
-    const body = JSON.parse(event.body);
-    const { text, language } = body;
-  };
+  }
+};
