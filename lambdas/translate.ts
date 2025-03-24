cf3443842b9b10931964aefc7aaeee9f5d16a904
SytxNox <seven_night_w@hotmail.com>
2025-03-24 12:36:28 +0000

    feat: implement trasnlate funciton

--- lambdas/translate.ts
+++ lambdas/translate.ts
@@ -15,9 +15,18 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     return apiResponses._400({ message: 'missing language from the body' });
   }
 
+  const translateParams: AWS.Translate.Types.TranslateTextRequest = {
+    Text: text,
+    SourceLanguageCode: 'en',
+    TargetLanguageCode: language,
+};
+
   try{
+    const translatedMessage = await translate.translateText(translateParams).promise();
+    return apiResponses._200({ translatedMessage });
 
   }catch(error){
-    
+    console.log('error in the translation', error);
+    return apiResponses._400({ message: 'unable to translate the message' });
   }
 };
